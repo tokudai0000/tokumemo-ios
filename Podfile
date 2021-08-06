6294413e01fe0644a0d1e17a58d3e2de0366c46c
Akihiro Matsuyama <a@art.jp>
2021-08-06 19:35:03 +0900

    初期設定

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'univIP' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for univIP
+  pod 'R.swift' #, '~> 5.1'               #, '5.1.0'
+
+end
---
+++ univIP/App/View/Main/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  univIP
+//
+//  Created by Akihiro Matsuyama on 2021/08/06.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        let vc = R.storyboard
+    }
+
+    @IBAction func CMButton(_ sender: Any) {
+//        let vc = R.storyboard.CM.CMview()!
+//        self.present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func ManabaButton(_ sender: Any) {
+    }
+    
+    @IBAction func SyllabusButton(_ sender: Any) {
+    }
+    
+    @IBAction func SettingsButton(_ sender: Any) {
+    }
+}
+
