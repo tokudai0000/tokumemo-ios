97a42756c8d05905a4bb8332e599213819048ffd
Akihiro Matsuyama <a@art.jp>
2021-10-08 16:10:56 +0900

    alert,confirm,prompt対応

--- univIP/App/View/Main/MainViewController.swift
+++ univIP/App/View/Main/MainViewController.swift
@@ -527,6 +527,54 @@ extension MainViewController: WKNavigationDelegate{
         // WebView表示、非表示　判定
         webViewHiddenJudge()
     }
+    // alert対応
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let otherAction = UIAlertAction(title: "OK", style: .default) {
+            action in completionHandler()
+        }
+        alertController.addAction(otherAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // confirm対応
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {
+            action in completionHandler(false)
+        }
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            action in completionHandler(true)
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // prompt対応
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        
+        // variable to keep a reference to UIAlertController
+        let alertController = UIAlertController(title: "", message: prompt, preferredStyle: .alert)
+        
+        let okHandler: () -> Void = {
+            if let textField = alertController.textFields?.first {
+                completionHandler(textField.text)
+            } else {
+                completionHandler("")
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {
+            action in completionHandler("")
+        }
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            action in okHandler()
+        }
+        alertController.addTextField() { $0.text = defaultText }
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
 
