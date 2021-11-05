1011cd481fa5f760ad677f9513101893b90ca5c9
Akihiro Matsuyama <a@art.jp>
2021-11-05 14:37:48 +0900

    リファクタ

--- univIP/App/View/Settings/AboutThisApp/AboutThisAppViewController.swift
+++ univIP/App/View/Settings/AboutThisApp/AboutThisAppViewController.swift
@@ -9,44 +9,20 @@
 import UIKit
 
 final class AboutThisAppViewController: BaseViewController {
-    //MARK:- IBOutlet
+    
+    // MARK: - IBOutlet
     @IBOutlet weak var textView: UITextView!
     
+    private let rtfFileModel = RtfFileModel()
     
-    //MARK:- LifeCycle
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         textView.isEditable = false
         textView.isSelectable = true
-        rtfFileOpen()
+        textView.attributedText = rtfFileModel.load(url: R.file.aboutThisAppRtf())
     }
     
-    
-    // MARK:- Private func
-    private func rtfFileOpen(){
-        if let url = R.file.aboutThisAppRtf() {
-            do {
-                let terms = try Data(contentsOf: url)
-                let attributedString = try NSAttributedString(data: terms,
-                                                             options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf],
-                                                             documentAttributes: nil)
-                
-                let linkFireBasePrivacy = (attributedString.string as NSString).range(of: "https://firebase.google.com/support/privacy?hl=ja")
-                let linkLicense = (attributedString.string as NSString).range(of: " akidon0000")
-                let linkSourceCode = (attributedString.string as NSString).range(of: "https://github.com/akidon0000/univIP")
-                let linkPrivacyPolicy = (attributedString.string as NSString).range(of: "https://github.com/akidon0000/univIP/blob/main/userPolicy.txt")
-                
-                let attributedText = NSMutableAttributedString(string: attributedString.string)
-                
-                attributedText.addAttribute(.link, value: "https://firebase.google.com/support/privacy?hl=ja", range: linkFireBasePrivacy)
-                attributedText.addAttribute(.link, value: "https://github.com/akidon0000/univIP/blob/main/LICENSE", range: linkLicense)
-                attributedText.addAttribute(.link, value: "https://github.com/akidon0000/univIP", range: linkSourceCode)
-                attributedText.addAttribute(.link, value: "https://github.com/akidon0000/univIP/blob/main/userPolicy.txt", range: linkPrivacyPolicy)
-                
-                textView.attributedText = attributedText
-            } catch let error {
-                print("ファイルの読み込みに失敗しました: \(error.localizedDescription)")
-            }
-        }
-    }
 }
