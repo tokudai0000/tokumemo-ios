c702fb98c77e99d2f9827fbb8923aaf7f6445595
Akihiro Matsuyama <a@art.jp>
2021-11-12 01:56:54 +0900

    リファクタ

--- univIP/App/Model/RtfFileModel.swift
+++ univIP/App/Model/RtfFileModel.swift
@@ -18,20 +18,12 @@ final class RtfFileModel {
                                                              options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf],
                                                              documentAttributes: nil)
                 return attributedString
-//                let linkSourceCode = (attributedString.string as NSString).range(of: "https://github.com/akidon0000/univIP")
-//                let linkFireBasePrivacy = (attributedString.string as NSString).range(of: "https://firebase.google.com/support/privacy?hl=ja")
-//
-//                let attributedText = NSMutableAttributedString(string: attributedString.string)
-//                attributedText.addAttribute(.link, value: "https://github.com/akidon0000/univIP", range: linkSourceCode)
-//                attributedText.addAttribute(.link, value: "https://firebase.google.com/support/privacy?hl=ja", range: linkFireBasePrivacy)
-//                return attributedText
                         
             } catch let error {
                 print("ファイルの読み込みに失敗しました: \(error.localizedDescription)")
             }
         }
         return NSAttributedString()
-//        return NSMutableAttributedString()
     }
     
 }
--- univIP/App/View/Agreement/AgreementViewController.swift
+++ univIP/App/View/Agreement/AgreementViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class AgreementViewController: BaseViewController, UITextViewDelegate {
     
-    //MARK:- IBOutlet
+    // MARK: - IBOutlet
     @IBOutlet weak var termsTextView: UITextView!
     @IBOutlet weak var agreementButtonYes: UIButton!
     
@@ -17,13 +17,41 @@ final class AgreementViewController: BaseViewController, UITextViewDelegate {
     private let rtfFileModel = RtfFileModel()
     let text = "トクメモのご利用規約またはプライバシーポリシーが更新されています。 \n サービスを継続してご利用するには、新しいご利用規約とプライバシーポリシーに同意する必要があります。"
     
-    //MARK:- LifeCycle
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        textViewSetup()
     }
     
     
+    // MARK: - IBAction
+    @IBAction func buttonAction(_ sender: Any) {
+        UserDefaults.standard.set(true, forKey: model.agreementVersion)
+        self.dismiss(animated: true, completion: nil)
+                
+    }
+    
+    
+    // MARK: - Public
+    public func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        let urlString = URL.absoluteString
+        if urlString == "TermsOfService" {
+            let vc = R.storyboard.termsOfService.termsOfService()!
+            self.present(vc, animated: true, completion: nil)
+            return false // 通常のURL遷移を行わない
+            
+        } else if urlString == "PrivacyPolicy" {
+            let vc = R.storyboard.privacyPolicy.privacyPolicy()!
+            self.present(vc, animated: true, completion: nil)
+            return false 
+            
+        }
+        return true // 通常のURL遷移を行う
+    }
+    
+    
+    // MARK: - Private
     private func setup() {
         agreementButtonYes.layer.cornerRadius = 20.0
         
@@ -32,7 +60,9 @@ final class AgreementViewController: BaseViewController, UITextViewDelegate {
         termsTextView.isSelectable = true
         termsTextView.delegate = self
         
-        
+    }
+    
+    private func textViewSetup() {
         let attributedString = NSMutableAttributedString(string: text)
         
         let linkSourceCode = (attributedString.string as NSString).range(of: "ご利用規約")
@@ -43,61 +73,6 @@ final class AgreementViewController: BaseViewController, UITextViewDelegate {
         attributedText.addAttribute(.link, value: "PrivacyPolicy", range: linkFireBasePrivacy)
         termsTextView.linkTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemTeal]
         termsTextView.attributedText = attributedText
-        
-    }
-    
-    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-        let urlString = URL.absoluteString
-        if urlString == "TermsOfService" {
-            let vc = R.storyboard.termsOfService.termsOfService()!
-            self.present(vc, animated: true, completion: nil)
-            return false // 通常のURL遷移を行わない
-            
-        } else if urlString == "PrivacyPolicy" {
-            let vc = R.storyboard.privacyPolicy.privacyPolicy()!
-            self.present(vc, animated: true, completion: nil)
-            return false // 通常のURL遷移を行わない
-            
-        }
-        return true // 通常のURL遷移を行う
-    }
-    
-    //MARK:- IBAction
-    private enum buttonTag: Int {
-        case agree = 1
-        case dissAgree = 2
-    }
-    
-    
-    @IBAction func buttonAction(_ sender: Any) {
-        if let button = sender as? UIButton,
-           let tag = buttonTag(rawValue: button.tag){
-            
-            switch tag {
-            case .agree:
-                UserDefaults.standard.set(true, forKey: model.agreementVersion)
-                dismiss(animated: false, completion: nil)
-                
-                
-            case .dissAgree:
-                toast(message: "同意をしない限り、このアプリを利用することはできません", type: "bottom")
-                
-            }
-        }
     }
     
 }
-
-//extension AgreementViewModel: UITextViewDelegate {
-//    public func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-//        let urlString = URL.absoluteString
-//        if urlString == "TermOfUseLink" {
-//            let vc = R.storyboard.termsOfService.termsOfService()!
-//            self.present(vc, animated: false, completion: nil)
-////            navigationController?.pushViewController(vc, animated: true)
-//            return false // 通常のURL遷移を行わない
-//
-//        }
-//        return true // 通常のURL遷移を行う
-//    }
-//}
