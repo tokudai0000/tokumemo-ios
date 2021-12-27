cf9feb8b1454230c5ff50e3e4ee82847ce3a6c8f
Akihiro Matsuyama <a@art.jp>
2021-12-27 19:32:22 +0900

    MainViewModelのUnitTest作成(修正必要)

--- univIPTests/MainViewModelTests.swift
+++ univIPTests/MainViewModelTests.swift
@@ -20,116 +20,117 @@ class MainViewModelTests: XCTestCase {
         super.tearDown()
     }
     
+    func testIsAllowedDomainCheck() {
+        
+        // 基本的なテスト
+        let url1 = URL(string: "https://tokushima-u.ac.jp/")! // fatalError
+        let test1 = viewModel.isAllowedDomainCheck(url1)
+        XCTAssertEqual(test1, true)
+
+        let url2 = URL(string: "https://microsoftonline.com/")!
+        let test2 = viewModel.isAllowedDomainCheck(url2)
+        XCTAssertEqual(test2, true)
+
+        let url3 = URL(string: "https://office365.com/")!
+        let test3 = viewModel.isAllowedDomainCheck(url3)
+        XCTAssertEqual(test3, true)
+
+        let url4 = URL(string: "https://office.com/")!
+        let test4 = viewModel.isAllowedDomainCheck(url4)
+        XCTAssertEqual(test4, true)
+
+        let url5 = URL(string: "https://youtube.com/")!
+        let test5 = viewModel.isAllowedDomainCheck(url5)
+        XCTAssertEqual(test5, true)
+
+        let url6 = URL(string: "https://example.com/")!
+        let test6 = viewModel.isAllowedDomainCheck(url6)
+        XCTAssertEqual(test6, false)
+        
+        
+        // 境界テスト
+        let url100 = URL(string: "https://tokushima-u.ac.jp/aaaa/aaaa/")!
+        let test100 = viewModel.isAllowedDomainCheck(url100)
+        XCTAssertEqual(test100, true)
+
+        let url101 = URL(string: "https://aaatokushima-u.ac.jp/")!
+        let test101 = viewModel.isAllowedDomainCheck(url101)
+        XCTAssertEqual(test101, true)
+
+        let url102 = URL(string: "https://takushima-u.ac.jp/")!
+        let test102 = viewModel.isAllowedDomainCheck(url102)
+        XCTAssertEqual(test102, false)
+
+
+        // 稼働テスト
+        let url201 = URL(string: "https://eweb.stud.tokushima-u.ac.jp/Portal/")!
+        let test201 = viewModel.isAllowedDomainCheck(url201)
+        XCTAssertEqual(test201, true)
+
+        let url202 = URL(string: "https://www.lib.tokushima-u.ac.jp/")!
+        let test202 = viewModel.isAllowedDomainCheck(url202)
+        XCTAssertEqual(test202, true)
+
+        let url203 = URL(string: "http://eweb.stud.tokushima-u.ac.jp/Portal/Public/Syllabus/")!
+        let test203 = viewModel.isAllowedDomainCheck(url203)
+        XCTAssertEqual(test203, true)
+
+        let url204 = URL(string: "https://login.microsoftonline.com/login")!
+        let test204 = viewModel.isAllowedDomainCheck(url204)
+        XCTAssertEqual(test204, true)
+
+        let url205 = URL(string: "https://login.microsoftonline.com/common/SAS/ProcessAuth")!
+        let test205 = viewModel.isAllowedDomainCheck(url205)
+        XCTAssertEqual(test205, true)
+
+        let url206 = URL(string: "https://outlook.office365.com/owa/")!
+        let test206 = viewModel.isAllowedDomainCheck(url206)
+        XCTAssertEqual(test206, true)
+
+        let url207 = URL(string: "https://www.tokudai-syusyoku.com/index.php")!
+        let test207 = viewModel.isAllowedDomainCheck(url207)
+        XCTAssertEqual(test207, true)
+
+        let url208 = URL(string: "https://manaba.lms.tokushima-u.ac.jp/ct/home")!
+        let test208 = viewModel.isAllowedDomainCheck(url208)
+        XCTAssertEqual(test208, true)
+    }
     
-//    func testIsDomeinCheck() {
-//
-//        // 基本的なテスト
-//        let url1 = "https://tokushima-u.ac.jp/"
-//        let test1 = viewModel.isAllowedDomeinCheck(url1)
-//        XCTAssertEqual(test1, true)
-//
-//        let url2 = "https://microsoftonline.com/"
-//        let test2 = viewModel.isAllowedDomeinCheck(url2)
-//        XCTAssertEqual(test2, true)
-//
-//        let url3 = "https://office365.com/"
-//        let test3 = viewModel.isAllowedDomeinCheck(url3)
-//        XCTAssertEqual(test3, true)
-//
-//        let url4 = "https://office.com/"
-//        let test4 = viewModel.isAllowedDomeinCheck(url4)
-//        XCTAssertEqual(test4, true)
-//
-//        let url5 = "https://youtube.com/"
-//        let test5 = viewModel.isAllowedDomeinCheck(url5)
-//        XCTAssertEqual(test5, true)
-//
-//        let url6 = "https://example.com/"
-//        let test6 = viewModel.isAllowedDomeinCheck(url6)
-//        XCTAssertEqual(test6, false)
-//
-//
-//        // 境界テスト
-//        let url100 = "https://tokushima-u.ac.jp/aaaa/aaaa/"
-//        let test100 = viewModel.isAllowedDomeinCheck(url100)
-//        XCTAssertEqual(test100, true)
-//
-//        let url101 = "https://aaatokushima-u.ac.jp/"
-//        let test101 = viewModel.isAllowedDomeinCheck(url101)
-//        XCTAssertEqual(test101, true)
-//
-//        let url102 = "https://takushima-u.ac.jp/"
-//        let test102 = viewModel.isAllowedDomeinCheck(url102)
-//        XCTAssertEqual(test102, false)
-//
-//
-//        // 稼働テスト
-//        let url201 = "https://eweb.stud.tokushima-u.ac.jp/Portal/"
-//        let test201 = viewModel.isAllowedDomeinCheck(url201)
-//        XCTAssertEqual(test201, true)
-//
-//        let url202 = "https://www.lib.tokushima-u.ac.jp/"
-//        let test202 = viewModel.isAllowedDomeinCheck(url202)
-//        XCTAssertEqual(test202, true)
-//
-//        let url203 = "http://eweb.stud.tokushima-u.ac.jp/Portal/Public/Syllabus/"
-//        let test203 = viewModel.isAllowedDomeinCheck(url203)
-//        XCTAssertEqual(test203, true)
-//
-//        let url204 = "https://login.microsoftonline.com/login"
-//        let test204 = viewModel.isAllowedDomeinCheck(url204)
-//        XCTAssertEqual(test204, true)
-//
-//        let url205 = "https://login.microsoftonline.com/common/SAS/ProcessAuth"
-//        let test205 = viewModel.isAllowedDomeinCheck(url205)
-//        XCTAssertEqual(test205, true)
-//
-//        let url206 = "https://outlook.office365.com/owa/"
-//        let test206 = viewModel.isAllowedDomeinCheck(url206)
-//        XCTAssertEqual(test206, true)
-//
-//        let url207 = "https://www.tokudai-syusyoku.com/index.php"
-//        let test207 = viewModel.isAllowedDomeinCheck(url207)
-//        XCTAssertEqual(test207, true)
-//
-//        let url208 = "https://manaba.lms.tokushima-u.ac.jp/ct/home"
-//        let test208 = viewModel.isAllowedDomeinCheck(url208)
-//        XCTAssertEqual(test208, true)
-//
-////        let url209 = URL(string: "")!
-////        let test209 = viewModel.isDomeinCheck(url209)
-////        XCTAssertEqual(test209, true)
-////
-////        let url210 = URL(string: "")!
-////        let test210 = viewModel.isDomeinCheck(url210)
-////        XCTAssertEqual(test210, true)
-////
-////        let url211 = URL(string: "")!
-////        let test211 = viewModel.isDomeinCheck(url211)
-////        XCTAssertEqual(test211, true)
-////
-////        let url212 = URL(string: "")!
-////        let test212 = viewModel.isDomeinCheck(url212)
-////        XCTAssertEqual(test212, true)
-////
-////        let url213 = URL(string: "")!
-////        let test213 = viewModel.isDomeinCheck(url213)
-////        XCTAssertEqual(test213, true)
+    func testIsFinishLogin() {
+        // 基本的なテスト
+        viewModel.isInitFinishLogin = true
+        let url1 = URL(string: Url.enqueteReminder.string())! // fatalError
+        let test1 = viewModel.isFinishLogin(url1)
+        XCTAssertEqual(test1, true)
+        
+        viewModel.isInitFinishLogin = true
+        let url2 = URL(string: Url.courseManagementMobile.string())!
+        let test2 = viewModel.isFinishLogin(url2)
+        XCTAssertEqual(test2, true)
+        
+        viewModel.isInitFinishLogin = false
+        let url3 = URL(string: Url.enqueteReminder.string())!
+        let test3 = viewModel.isFinishLogin(url3)
+        XCTAssertEqual(test3, true)
+        
+        viewModel.isInitFinishLogin = false
+        let url4 = URL(string: Url.courseManagementMobile.string())!
+        let test4 = viewModel.isFinishLogin(url4)
+        XCTAssertEqual(test4, false)
+        
+    }
+    
+
+    // dataManagerを内部で使用している為、モックか修正を行う
+//    func testAnyJavaScriptExecute() {
+//    }
+//    func testSearchInitialViewUrl() {
 //
 //    }
-//
-//    func testIsJudgeUrl() {
-//        do {
-//            let isRegistrant = true
-//            let forwardUrl = ""
-//            let displayUrl = "https://localidp.ait230.tokushima-u.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1"
-//            let test = viewModel.isJudgeUrl(.login,
-//                                            isRegistrant: isRegistrant,
-//                                            forwardUrl: forwardUrl,
-//                                            displayUrl: displayUrl)
-//            XCTAssertEqual(test, true)
-//        }
-//    }
     
+    // これは大学図書館ホームページの使用により、頻繁に返り値が変わる。修正
+//    func testFetchLibraryCalendarUrl() {
+//
+//    }
     
 }
