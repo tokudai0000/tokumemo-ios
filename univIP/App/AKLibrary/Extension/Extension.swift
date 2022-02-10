1599718073c90b12e06a40d25a27c0373941c74e
Akihiro Matsuyama <a@art.jp>
2022-02-11 01:28:46 +0900

    Extension作成　Date

--- univIP/App/AKLibrary/Extension/Extension.swift
+++ univIP/App/AKLibrary/Extension/Extension.swift
@@ -4,26 +4,21 @@
 //
 //  Created by Akihiro Matsuyama on 2022/02/11.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+extension DateFormatter {
+    // テンプレートの定義(例)
+    enum Template: String {
+        case date = "yMd"     // 2017/1/1
+        case time = "Hms"     // 12:39:22
+        case full = "yMdkHms" // 2017/1/1 12:39:22
+        case onlyHour = "k"   // 17時
+        case era = "GG"       // "西暦" (default) or "平成" (本体設定で和暦を指定している場合)
+        case weekDay = "EEEE" // 日曜日
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setTemplate(_ template: Template) {
+        // optionsは拡張用の引数だが使用されていないため常に0
+        dateFormat = DateFormatter.dateFormat(fromTemplate: template.rawValue, options: 0, locale: .current)
     }
-    */
-
 }
