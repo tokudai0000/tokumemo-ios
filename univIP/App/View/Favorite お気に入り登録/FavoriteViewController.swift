da31c4bdc535630356ed0390803024fa839292b7
Akihiro Matsuyama <a@art.jp>
2022-02-16 09:32:15 +0900

    お気に入り登録画面の作成

---
+++ univIP/App/View/Favorite お気に入り登録/FavoriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteViewController.swift
+//  univIP
+//
+//  Created by Akihiro Matsuyama on 2022/02/16.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
