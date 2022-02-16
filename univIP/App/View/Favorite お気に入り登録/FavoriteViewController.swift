afda0b3879e3c709bff5b015a626e1402c75a80b
Akihiro Matsuyama <a@art.jp>
2022-02-16 10:02:54 +0900

    お気に入り登録、ストーリーボードと接続

--- univIP/App/View/Favorite お気に入り登録/FavoriteViewController.swift
+++ univIP/App/View/Favorite お気に入り登録/FavoriteViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
-
+    
+    @IBOutlet weak var urlLabel: UILabel!
+    @IBOutlet weak var favoriteTextSizeLabel: UILabel!
+    @IBOutlet weak var favoriteNameTextField: UITextField!
+    @IBOutlet weak var isFirstViewSetting: UISwitch!
+    
+    public var urlString = "nil"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        urlLabel.text = urlString
+    }
 
-        // Do any additional setup after loading the view.
+    // MARK: - IBAction
+    @IBAction func registerButton(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
