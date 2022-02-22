3a0d240a9905c78e2cb857ed871404bcd2c6b3dc
Akihiro Matsuyama <a@art.jp>
2022-02-22 15:40:54 +0900

    Splash作成

---
+++ univIP/App/View/Splash/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  univIP
+//
+//  Created by Akihiro Matsuyama on 2021/08/06.
+//  Copyright © 2021年　akidon0000
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // アナリティクスをシュミレーターでは起動させない
+        #if !targetEnvironment(simulator)
+            FirebaseApp.configure()
+        #endif
+        
+        return true
+    }
+}
