3e649f296ac1245737a3e19e50316e2547528561
Keita Miyake <suvalism@gmail.com>
2022-10-27 03:24:58 +0900

    学生証の画面作成

---
+++ univIP/App/View/Main メイン/Home ホーム/StudentCard 学生証/StudentCardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudentCardViewController.swift
+//  univIP
+//
+//  Created by Keita Miyake on 2022/10/26.
+//
+
+import UIKit
+
+class StudentCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
