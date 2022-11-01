b6f43d06fce087e1df515505a4abd250a2901fe8
Keita Miyake <suvalism@gmail.com>
2022-11-01 19:54:09 +0900

    学生証表示機能追加

--- univIP/App/View/Main メイン/Home ホーム/StudentCard 学生証/StudentCardViewController.swift
+++ univIP/App/View/Main メイン/Home ホーム/StudentCard 学生証/StudentCardViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class StudentCardViewController: UIViewController {
 
+    @IBOutlet weak var BarCodeImageView: UIImageView!
+    @IBOutlet weak var StudentNumberLabel: UILabel!
+    
+    // バーコードを生成する
+    let GeneratedImage = BarCodeGenerator.generateBarCode(from: "1234567890")
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 画像を読み込む
+        BarCodeImageView.image = GeneratedImage
+        
+        // Labelを変更
+        StudentNumberLabel.text = "A1234567890A"
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
