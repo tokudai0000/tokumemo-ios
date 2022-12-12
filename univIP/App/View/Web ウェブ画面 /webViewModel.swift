e90793d8ce4ab9603b3eb2877b9f959c370da883
Akihiro Matsuyama(MacBookPro) <a@art.jp>
2022-12-12 23:05:51 +0900

    [modify]WebViewでも自動ログインを復活

--- univIP/App/View/Web ウェブ画面 /webViewModel.swift
+++ univIP/App/View/Web ウェブ画面 /webViewModel.swift
@@ -32,21 +32,19 @@ final class WebViewModel {
     /// - Note: canExecuteJavascriptが重要な理由
     ///   ログインに失敗した場合に再度ログインのURLが表示されることになる。
     ///   canExecuteJavascriptが存在しないと、再度ログインの為にJavaScriptが実行され続け無限ループとなってしまう。
-    /// - Parameter urlString: 読み込み完了したURLの文字列
-    /// - Returns: 動かすJavaScriptの種類
     public func anyJavaScriptExecute(_ urlString: String) -> JavaScriptType {
         // JavaScriptを実行するフラグが立っていない場合はnoneを返す
         if dataManager.canExecuteJavascript == false {
             return .none
         }
         // アンケート解答の催促画面
-        //        if urlString == Url.skipReminder.string() {
-        //            return .skipReminder
-        //        }
+        if urlString == Url.skipReminder.string() {
+            return .skipReminder
+        }
         // 大学統合認証システム(IAS)のログイン画面
-        //        if urlString.contains(Url.universityLogin.string()) {
-        //            return .loginIAS
-        //        }
+        if urlString.contains(Url.universityLogin.string()) {
+            return .loginIAS
+        }
         // シラバスの検索画面
         if urlString == Url.syllabus.string() {
             return .syllabus
