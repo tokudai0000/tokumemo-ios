4c0ab16c5863eec37d4ccc37a92b42625a1cf613
akidon0000 <a@art.jp>
2023-01-04 21:31:15 +0900

    [Modify] 不具合修正、表示完了

--- univIP/App/View/Main メイン/Review レビュー/ReviewViewController.swift
+++ univIP/App/View/Main メイン/Review レビュー/ReviewViewController.swift
@@ -13,13 +13,21 @@ class ReviewViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
     
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        webView.load(Url.clubList.urlRequest())
+        webView.configuration.userContentController.add(self, name: "test1")
         
         // ステータスバーの背景色を指定
         setStatusBarBackgroundColor(UIColor(red: 13/255, green: 58/255, blue: 151/255, alpha: 1.0))
+    }
+    
+    /// 画面が表示される直前
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        webView.load(Url.clubList.urlRequest())
     }
     
     // ステータスバーのスタイルを白に設定
@@ -27,10 +35,19 @@ class ReviewViewController: UIViewController {
             return .lightContent
     }
     
+//    override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+//        switch item.tag {
+//            case 1:
+//              println("foo")
+//            default:
+//              println("bar")
+//        }
+//    }
+    
 }
 
 // MARK: - WebView
-extension ReviewViewController: WKNavigationDelegate {
+extension ReviewViewController: WKNavigationDelegate, WKScriptMessageHandler {
     // ログイン用に使用するWebViewについての設定
     
     /// 読み込み設定（リクエスト前）
@@ -38,12 +55,11 @@ extension ReviewViewController: WKNavigationDelegate {
                  decidePolicyFor navigationAction: WKNavigationAction,
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         
-        guard let url = navigationAction.request.url else {
+        guard navigationAction.request.url != nil else {
             AKLog(level: .FATAL, message: "読み込み前のURLがnil")
             decisionHandler(.cancel)
             return
         }
-        
         
         decisionHandler(.allow)
         return
@@ -53,5 +69,12 @@ extension ReviewViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         
         
+    }
+    
+    /// Web側(JavaScript)から値渡し
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        let vc = R.storyboard.web.webViewController()!
+        vc.loadUrlString = message.body as? String
+        present(vc, animated: true, completion: nil)
     }
 }
