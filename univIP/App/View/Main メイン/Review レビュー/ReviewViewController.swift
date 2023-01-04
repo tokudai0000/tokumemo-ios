4b38ea813f532e5d24ad971158b6ba183aeed358
akidon0000 <a@art.jp>
2023-01-04 19:22:50 +0900

    [Add]ClubListの表示

--- univIP/App/View/Main メイン/Review レビュー/ReviewViewController.swift
+++ univIP/App/View/Main メイン/Review レビュー/ReviewViewController.swift
@@ -7,14 +7,16 @@
 
 import UIKit
 import StoreKit
+import WebKit
 
 class ReviewViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-//            SKStoreReviewController.requestReview(in: scene)
-//        }
+        
+        webView.load(Url.clubList.urlRequest())
         
         // ステータスバーの背景色を指定
         setStatusBarBackgroundColor(UIColor(red: 13/255, green: 58/255, blue: 151/255, alpha: 1.0))
@@ -25,4 +27,31 @@ class ReviewViewController: UIViewController {
             return .lightContent
     }
     
+}
+
+// MARK: - WebView
+extension ReviewViewController: WKNavigationDelegate {
+    // ログイン用に使用するWebViewについての設定
+    
+    /// 読み込み設定（リクエスト前）
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        guard let url = navigationAction.request.url else {
+            AKLog(level: .FATAL, message: "読み込み前のURLがnil")
+            decisionHandler(.cancel)
+            return
+        }
+        
+        
+        decisionHandler(.allow)
+        return
+    }
+    
+    /// 読み込み完了
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        
+    }
 }
