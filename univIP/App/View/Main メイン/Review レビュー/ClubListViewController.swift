e01a0cac508a5a482904f4fc05756e7d52e76db1
akidon0000 <a@art.jp>
2023-01-05 00:44:08 +0900

    [Modify]ClubList名に変更

--- univIP/App/View/Main メイン/Review レビュー/ClubListViewController.swift
+++ univIP/App/View/Main メイン/Review レビュー/ClubListViewController.swift
@@ -6,19 +6,17 @@
 //
 
 import UIKit
-import StoreKit
 import WebKit
 
-class ReviewViewController: UIViewController {
+class ClubListViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
-    
-    @IBOutlet weak var mainView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        webView.configuration.userContentController.add(self, name: "test1")
+        // Web側(JavaScript)からtokumemoPlus関数が送られてくるのを受け取る設定
+        webView.configuration.userContentController.add(self, name: "tokumemoPlus")
         
         // ステータスバーの背景色を指定
         setStatusBarBackgroundColor(UIColor(red: 13/255, green: 58/255, blue: 151/255, alpha: 1.0))
@@ -35,19 +33,10 @@ class ReviewViewController: UIViewController {
             return .lightContent
     }
     
-//    override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
-//        switch item.tag {
-//            case 1:
-//              println("foo")
-//            default:
-//              println("bar")
-//        }
-//    }
-    
 }
 
 // MARK: - WebView
-extension ReviewViewController: WKNavigationDelegate, WKScriptMessageHandler {
+extension ClubListViewController: WKNavigationDelegate, WKScriptMessageHandler {
     // ログイン用に使用するWebViewについての設定
     
     /// 読み込み設定（リクエスト前）
@@ -74,6 +63,8 @@ extension ReviewViewController: WKNavigationDelegate, WKScriptMessageHandler {
     /// Web側(JavaScript)から値渡し
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         let vc = R.storyboard.web.webViewController()!
+        // postMessageを受けとり、表示する
+        // window.webkit.messageHandlers.tokumemoPlus.postMessage(url)
         vc.loadUrlString = message.body as? String
         present(vc, animated: true, completion: nil)
     }
