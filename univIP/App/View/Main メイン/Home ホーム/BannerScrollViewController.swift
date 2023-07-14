5f34bbd4cc944150fce9280783d077558d29ff1f
akidon0000 <a@art.jp>
2023-07-14 11:47:02 +0900

    [Add]カルーセルを作成、デモとしてカラー画像を表示させた

---
+++ univIP/App/View/Main メイン/Home ホーム/BannerScrollViewController.swift
@@ -0,0 +1,190 @@
+//
+//  BannerScrollViewController.swift
+//  univIP
+//
+//  Created by Akihiro Matsuyama on 2023/07/14.
+//
+
+import UIKit
+
+protocol BannerScrollViewControllerDelegate: AnyObject {
+    /// pageIndexが変わったときに呼ばれる
+    func bannerScrollViewController(_ scrollViewController: BannerScrollViewController, didChangePageIndex index: Int)
+}
+
+/// バナー表示Viewのコントローラ
+/// scrollViewを使って表示している
+class BannerScrollViewController: UIViewController {
+    /// バナーエリアの全体の幅
+    let baseComponentWidth: CGFloat = 406
+    /// デザインでのパネルの幅
+    let basePanelWidth: CGFloat = 355
+    /// デザインでのパネルの高さ
+    let basePanelImageHeight: CGFloat = 238
+    /// デザインでのテキスト部分の高さ
+    let basePanelTextContainerHeight: CGFloat = 30
+    /// デザインでのパネルの横マージン（先頭の左マージン）
+    lazy var basePanelHorizontalMargin: CGFloat = (baseComponentWidth - basePanelWidth) / 2
+    /// デザインでのパネル間のマージン
+    let basePanelGap: CGFloat = 8
+    /// パネルの幅
+    private(set) var panelWidth: CGFloat = 0
+    /// パネルの高さ
+    private(set) var panelHeight: CGFloat = 0
+    /// パネルの横マージン（先頭の左マージン）
+    private(set) var panelHorizontalMargin: CGFloat = 0
+    /// パネル間のマージン
+    private(set) var panelGap: CGFloat = 0
+
+    private(set) var scrollView: UIScrollView!
+
+    private(set) var contentView: UIView!
+    private var contentViewWidthConstraint: NSLayoutConstraint!
+
+    /// 現在のPageIndex
+    private(set) var pageIndex: Int = 0
+
+    var delegate: BannerScrollViewControllerDelegate?
+
+    //開発用のダミーデータ
+    let colorHexs: [String] = ["D4C2FC", "C2DDFC", "C2FCF4", "C2FCCC", "D4FCC2",
+                               "EEFCC2", "FCE9C2", "FBC2A5", "FBAEA5", "FBA5CF"]
+    lazy var colors: [UIColor] = {
+        colorHexs.map { UIColor.hexStringToUIColor(hex: $0) }
+    }()
+
+    /// 初期化
+    func setup() {
+        setupSizes()
+        setupScrollView()
+        setupContentSize()
+        addPanels()
+    }
+
+    /// viewの大きさ測定
+    func setupSizes() {
+        let viewWidth = view.frame.width
+        let ratio: CGFloat = viewWidth / baseComponentWidth
+        panelWidth = ratio * basePanelWidth
+        panelHeight = ratio * basePanelImageHeight + basePanelTextContainerHeight
+        panelHorizontalMargin = ratio * basePanelHorizontalMargin
+        panelGap = ratio * basePanelGap
+    }
+
+    func setupScrollView() {
+        scrollView = UIScrollView()
+        scrollView.delegate = self
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+        // 本来見えない箇所をclipsToBounds=falseで見えるようにしている。
+        scrollView.clipsToBounds = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+
+        // 横幅はパネルの幅+パネル間のマージン
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: panelHorizontalMargin).isActive = true
+        view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: (panelHorizontalMargin - panelGap)).isActive = true
+        // frameの外はタッチイベントがきかない。その回避策
+        // https://stackoverflow.com/a/36641652
+        view.addGestureRecognizer(scrollView.panGestureRecognizer)
+
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        contentViewWidthConstraint = contentView.widthAnchor.constraint(equalToConstant: 0)
+        contentViewWidthConstraint.isActive = true
+        contentView.heightAnchor.constraint(equalToConstant: panelHeight).isActive = true
+    }
+
+    func setupContentSize() {
+        let count = CGFloat(colors.count)
+        let contentWidth = (panelWidth + panelGap) * (count)
+        contentViewWidthConstraint.constant = contentWidth
+        scrollView.contentSize = CGSize(width: contentWidth, height: panelHeight)
+    }
+
+    var bannerView: BannerView!
+
+    /// パネル追加
+    func addPanels() {
+        var prevBannerView: BannerView?
+        colors.enumerated().forEach { index, color in
+            if let bannerView =  UINib(nibName: "BannerView", bundle: nil).instantiate(withOwner: nil, options: nil).first as? BannerView {
+                // 中身表示
+                bannerView.backgroundColor = color
+//                bannerView.titleLabel.text = "これはサンプルです。バナーNo. \(index + 1)"
+//                bannerView.subTextLabel.text = "サブテキストが入ります。あいうえお"
+
+                // レイアウト
+                bannerView.translatesAutoresizingMaskIntoConstraints = false
+                contentView.addSubview(bannerView)
+                bannerView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+                bannerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+                if index == 0 {
+                    bannerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+                } else {
+                    if let prevBanner = prevBannerView {
+                        bannerView.leadingAnchor.constraint(equalTo: prevBanner.trailingAnchor, constant: panelGap).isActive = true
+                    }
+                }
+                bannerView.widthAnchor.constraint(equalToConstant: panelWidth).isActive = true
+                prevBannerView = bannerView
+
+                // バナーをタップしたときのイベント設定
+                let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapBanner(gesture:)))
+                gesture.numberOfTouchesRequired = 1
+                bannerView.tag = index
+                bannerView.addGestureRecognizer(gesture)
+            }
+        }
+        contentView.layoutSubviews()
+    }
+
+    @objc func didTapBanner(gesture: UITapGestureRecognizer) {
+        if case .ended = gesture.state,
+           let index = gesture.view?.tag {
+
+            let alertController = UIAlertController(title: "\(index) がタップされました", message: nil, preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default) { _ in
+                alertController.dismiss(animated: true, completion: nil)
+            }
+            alertController.addAction(okButton)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+
+    /// 現在のページindexを返す
+    func calcPageIndex(contentOffsetX: CGFloat) -> Int {
+        // 0.5 - 1.5 の間は1で返す
+        // 1.5 - 2.5 の間は2で返す....
+        return Int(round(contentOffsetX / (panelWidth + panelGap)))
+    }
+
+    /// ページを表示する
+    func showPage(index: Int, animated: Bool) {
+        let offset = CGPoint(
+            x: CGFloat(index) * (panelWidth + panelGap),
+            y: 0
+        )
+        scrollView.setContentOffset(offset, animated: animated)
+    }
+}
+
+extension BannerScrollViewController: UIScrollViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let currentIndex = pageIndex
+        let newIndex = calcPageIndex(contentOffsetX: scrollView.contentOffset.x)
+        if currentIndex != newIndex {
+            pageIndex = newIndex
+            delegate?.bannerScrollViewController(self, didChangePageIndex: newIndex)
+        }
+    }
+}
--- univIP/App/View/Main メイン/Home ホーム/HomeContainerViewController.swift
+++ univIP/App/View/Main メイン/Home ホーム/HomeContainerViewController.swift
@@ -9,40 +9,90 @@ import UIKit
 
 class HomeContainerViewController: UIViewController {
 
-    @IBOutlet weak var prImageCollectionView: UICollectionView!
-    let prImageCollerctionVC = PrImageCollectionViewController()
+    @IBOutlet weak var bannerContainerView: UIView!
+    @IBOutlet weak var bannerContainerViewHeightConstraint: NSLayoutConstraint!
+    var bannerViewController: BannerScrollViewController!
+
+    @IBOutlet weak var bannerPageControl: UIPageControl!
+
+//    @IBOutlet weak var prImageCollectionView: UICollectionView!
+//    let prImageCollerctionVC = PrImageCollectionViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bannerViewController = BannerScrollViewController()
+        addChild(bannerViewController)
+        bannerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        bannerContainerView.addSubview(bannerViewController.view)
+        bannerViewController.view.topAnchor.constraint(equalTo: bannerContainerView.topAnchor).isActive = true
+        bannerViewController.view.bottomAnchor.constraint(equalTo: bannerContainerView.bottomAnchor).isActive = true
+        bannerViewController.view.leadingAnchor.constraint(equalTo: bannerContainerView.leadingAnchor).isActive = true
+        bannerViewController.view.trailingAnchor.constraint(equalTo: bannerContainerView.trailingAnchor).isActive = true
+        bannerViewController.didMove(toParent: self)
+        bannerViewController.setup()
+        bannerContainerViewHeightConstraint.constant = bannerViewController.panelHeight
+        bannerViewController.delegate = self
 
-        prImageCollectionView.register(R.nib.homeCollectionCell)
-
-        prImageCollectionView.delegate = prImageCollerctionVC
-        prImageCollectionView.dataSource = prImageCollerctionVC
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 100)
-        prImageCollectionView.collectionViewLayout = layout
-    }
-
-}
-
-class PrImageCollectionViewController: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return initMenuLists.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: R.nib.homeCollectionCell, for: indexPath)!
-        let collectionList = initMenuLists[indexPath.row]
-        let title = collectionList.title
-        let icon = collectionList.image
-        cell.setupCell(title: title, image: icon)
-        return cell
-    }
-}
-
-//extension HomeContainerViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+        setupBannerPageControl()
+//        prImageCollectionView.register(R.nib.homeCollectionCell)
 //
+//        prImageCollectionView.delegate = prImageCollerctionVC
+//        prImageCollectionView.dataSource = prImageCollerctionVC
+//        let layout = UICollectionViewFlowLayout()
+//        layout.itemSize = CGSize(width: 100, height: 100)
+//        prImageCollectionView.collectionViewLayout = layout
+    }
+
+    func setupBannerPageControl() {
+        bannerPageControl.pageIndicatorTintColor = .lightGray
+        bannerPageControl.currentPageIndicatorTintColor = .black
+        bannerPageControl.numberOfPages = bannerViewController.colors.count
+        bannerPageControl.currentPage = bannerViewController.pageIndex
+        bannerPageControl.sizeToFit()
+    }
+
+    func updateBannerPageControl() {
+        bannerPageControl.currentPage = bannerViewController.pageIndex
+    }
+
+    @IBAction func didChangeBannerPageControl() {
+        bannerViewController.showPage(index: bannerPageControl.currentPage, animated: true)
+    }
+}
+extension HomeContainerViewController: BannerScrollViewControllerDelegate {
+
+    func bannerScrollViewController(_ scrollViewController: BannerScrollViewController, didChangePageIndex index: Int) {
+        updateBannerPageControl()
+    }
+}
+
+extension UIColor {
+
+    /// https://stackoverflow.com/a/27203691
+    static func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
+//class PrImageCollectionViewController: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 //        return initMenuLists.count
 //    }
@@ -55,6 +105,4 @@ class PrImageCollectionViewController: NSObject, UICollectionViewDelegate, UICol
 //        cell.setupCell(title: title, image: icon)
 //        return cell
 //    }
-//
-//
 //}
