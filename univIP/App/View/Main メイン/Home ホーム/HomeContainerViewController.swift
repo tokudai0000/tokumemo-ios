6d96ff858bb090f20239a1798f4c01b09249f349
akidon0000 <a@art.jp>
2023-07-14 10:31:26 +0900

    [Add]HomeContainerViewの作成

---
+++ univIP/App/View/Main メイン/Home ホーム/HomeContainerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeContainerViewController.swift
+//  univIP
+//
+//  Created by Akihiro Matsuyama on 2023/07/14.
+//
+
+import UIKit
+
+class HomeContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
