086d22a8884a4667ffcbf589d9233ae5facebcc1
akidon0000 <a@art.jp>
2023-07-20 14:41:00 +0900

    [Fix]メニューリストの内容を修正

--- univIP/App/Model/MenuList.swift
+++ univIP/App/Model/MenuList.swift
@@ -44,26 +44,37 @@ public struct MenuItemList: Codable {
 
 let homeMenuLists:[MenuItemList] = [
     MenuItemList(title: "教務システム", id: .courseManagement, image: R.image.menuIcon.courseManagementHome.name, url: Url.courseManagementMobile.string()),
-
     MenuItemList(title: "manaba", id: .manaba, image: R.image.menuIcon.manaba.name, url: Url.manabaPC.string()),
-
     MenuItemList(title: "メール", id: .mailService, image: R.image.menuIcon.mailService.name, url: Url.outlookService.string()),
-
     MenuItemList(title: "図書館 関連", id: .libraryRAS, image: R.image.menuIcon.libraryBookLendingExtension.name, url: nil),
-
     MenuItemList(title: "生協 関連", id: .coopRAS, image: R.image.menuIcon.coopCalendar.name, url: nil),
-
     MenuItemList(title: "その他", id: .etc, image: R.image.menuIcon.careerCenter.name, url: nil)
 ]
 
 let homelibraryRASItemLists = [
-    MenuItemList(title: "a生協 関連", id: .coopRAS, image: R.image.menuIcon.coopCalendar.name, url: Url.manabaPC.string()),
+    MenuItemList(title: "図書館ホームページ(常三島)", id: .libraryWebHomePC, image: "", url: Url.libraryHomePageMainPC.string()),
+    MenuItemList(title: "図書館ホームページ(蔵本)", id: .libraryWebHomeKuraPC, image: "", url: Url.libraryHomePageKuraPC.string()),
+    MenuItemList(title: "マイページ", id: .libraryMyPage, image: "", url: Url.libraryMyPage.string()),
+    MenuItemList(title: "貸し出し期間延長", id: .libraryBookLendingExtension, image: "", url: Url.libraryBookLendingExtension.string()),
+    MenuItemList(title: "購入リクエスト", id: .libraryBookPurchaseRequest, image: "", url: Url.libraryBookPurchaseRequest.string()),
+    MenuItemList(title: "[図書]カレンダー", id: .libraryCalendar, image: R.image.menuIcon.libraryCalendar.name, url: nil),
 ]
 
 let homeCoopRASItemLists = [
-    MenuItemList(title: "s生協 関連", id: .coopRAS, image: R.image.menuIcon.coopCalendar.name, url: Url.manabaPC.string()),
+    MenuItemList(title: "時間割", id: .timeTable, image: R.image.menuIcon.timetable.name, url: Url.timeTable.string()),
+    MenuItemList(title: "今学期の成績", id: .currentTermPerformance, image: "", url: Url.currentTermPerformance.string()),
+    MenuItemList(title: "シラバス", id: .syllabus, image: "", url: Url.syllabus.string()),
+    MenuItemList(title: "全学期の成績", id: .termPerformance, image: "", url: Url.termPerformance.string()),
+    MenuItemList(title: "大学サイト", id: .universityWeb, image: "", url: Url.universityHomePage.string()),
+    MenuItemList(title: "出欠記録", id: .presenceAbsenceRecord, image: "", url: Url.presenceAbsenceRecord.string()),
+    MenuItemList(title: "授業アンケート", id: .classQuestionnaire, image: "", url: Url.classQuestionnaire.string()),
+    MenuItemList(title: "LMS一覧", id: .eLearningList, image: "", url: Url.eLearningList.string()),
 ]
 
 let homeEtcItemLists = [
-    MenuItemList(title: "d生協 関連", id: .coopRAS, image: R.image.menuIcon.coopCalendar.name, url: Url.manabaPC.string()),
+    MenuItemList(title: "生協営業時間", id: .coopCalendar, image: "", url: Url.tokudaiCoop.string()),
+    MenuItemList(title: "食堂メニュー", id: .cafeteria, image: "", url: Url.tokudaiCoopDinigMenu.string()),
+    MenuItemList(title: "キャリア支援室", id: .careerCenter, image: "", url: Url.tokudaiCareerCenter.string()),
+    MenuItemList(title: "SSS時間割", id: .studySupportSpace, image: "", url: Url.studySupportSpace.string()),
+    MenuItemList(title: "知っておきたい防災", id: .disasterPrevention, image: "", url: Url.disasterPrevention.string()),
 ]
