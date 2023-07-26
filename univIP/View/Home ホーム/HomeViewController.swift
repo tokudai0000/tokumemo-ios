f6fd45b40d779f25f20966e81ae4ac4ce9775acd
akidon0000 <a@art.jp>
2023-07-26 23:56:40 +0900

    [temp]レイアウト配置成功

--- univIP/View/Home ホーム/HomeViewController.swift
+++ univIP/View/Home ホーム/HomeViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+
+    // Do any additional setup after loading the view.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.contentSize = containerView.frame.size
+        print("containerView:\(containerView.frame.size)")
+    }
+
 }
