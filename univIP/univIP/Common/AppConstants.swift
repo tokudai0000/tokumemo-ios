3c932be574fa1a398e67dc5b9905be28937ff393
akidon0000 <a@art.jp>
2023-09-17 15:15:03 +0900

    アプリ内に規約バージョンを保持し、UDと比較する

---
+++ univIP/univIP/Common/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  univIP
+//
+//  Created by Akihiro Matsuyama on 2023/09/17.
+//
+
+enum AppConstants {
+    // 利用規約のバージョン
+    static let termsOfServiceVersion = "3.1.0"
+
+}
