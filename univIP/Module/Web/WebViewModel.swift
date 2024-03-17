262bebb78e926b746a70d3400bf81cf299476fdc
akidon0000 <akidon0000@gmail.com>
2024-03-17 17:53:43 +0900

    [179] modify display urlLabel

--- univIP/Module/Web/WebViewModel.swift
+++ univIP/Module/Web/WebViewModel.swift
@@ -111,12 +111,9 @@ final class WebViewModel: BaseViewModel<WebViewModel>, WebViewModelInterface {
 
         input.urlPendingLoad
             .subscribe { url in
-                guard let url = url.element,
-                      let host = url.host else {
+                guard let url = url.element else {
                     return
                 }
-                AKLog(level: .DEBUG, message: "URL: \(url)")
-                urlLabel.accept(host.description)
 
                 if URLCheckers.isUniversityServiceTimeoutURL(at: url.absoluteString) {
                     reloadLoginURLInWebView.accept(Void())
@@ -127,9 +124,12 @@ final class WebViewModel: BaseViewModel<WebViewModel>, WebViewModelInterface {
         input.urlDidLoad
             .subscribe { url in
                 guard let url = url.element,
+                      let host = url.host,
                       let canExecuteJavascript = state.canExecuteJavascript.value else {
                     return
                 }
+                urlLabel.accept(host.description)
+                AKLog(level: .DEBUG, message: "urlDidLoad: \(url)")
 
                 if URLCheckers.isSkipReminderURL(at: url.absoluteString) {
                     skipReminderJavaScriptInjection.accept(Void())
